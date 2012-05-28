ec1801fce3d0b2e69b42f810270b08e5110c7c28
lepture <lepture@me.com>
2012-05-28 16:20:58 +0800

    add seajs demo

--- assets/demo.js
+++ assets/demo.js
@@ -1,37 +1,46 @@
-// export to jQuery
-$.fn.selection = selection;
+(function(){
+    function demoCase($, selection) {
+        var demo = $('#demo');
+        var sel = selection(demo);
+        demo.on('click select keyup', function(){
+            $('#explain1').text(sel.text());
+            $('#explain2').text(sel.cursor());
+            $('#explain3').text(sel.surround());
+        });
 
-$(function(){
-    var demo = $('#demo');
-    var sel = $.fn.selection(demo);
-    demo.on('click select keyup', function(){
-        $('#explain1').text(sel.text());
-        $('#explain2').text(sel.cursor());
-        $('#explain3').text(sel.surround());
-    });
+        $('#trigger2').click(function() {
+            sel.cursor(5, 9);
+            return false;
+        });
 
-    $('#trigger2').click(function() {
-        sel.cursor(5, 9);
-        return false;
-    });
+        $('#trigger4').click(function() {
+            sel.text('this is a replaced text');
+            return false;
+        });
 
-    $('#trigger4').click(function() {
-        sel.text('this is a replaced text');
-        return false;
-    });
+        $('#trigger5').click(function() {
+            sel.prepend('this is a prepend text');
+            return false;
+        });
 
-    $('#trigger5').click(function() {
-        sel.prepend('this is a prepend text');
-        return false;
-    });
+        $('#trigger6').click(function() {
+            sel.append('this is an append text');
+            return false;
+        });
 
-    $('#trigger6').click(function() {
-        sel.append('this is an append text');
-        return false;
-    });
-
-    $('#trigger7').click(function() {
-        alert(sel.line());
-        return false;
-    });
-});
+        $('#trigger7').click(function() {
+            alert(sel.line());
+            return false;
+        });
+    }
+    if (typeof jQuery !== 'undefined') {
+        $(function(){
+            demoCase($, selection);
+        });
+    } else {
+        // seajs
+        seajs.use(['jquery', './selection'], function($, selection) {
+            demoCase($, selection);
+        });
+    }
+})();
