433b2a4c05ffd42117631797c489880315b9faaa
Hsiaoming Yang <lepture@me.com>
2012-12-14 11:25:24 -0800

    Update documentation

--- static/js/arale-helper.js
+++ static/js/arale-helper.js
@@ -2,19 +2,18 @@
   var CDN_MODULES = [
     'jquery', 'zepto', 'json', 'jasmine', 'underscore', 'handlebars',
     'seajs', 'moment', 'async', 'store', 'swfobject', 'backbone', 'raphael'
-  ]
+  ];
 
-  var ALIPAY_BASE = 'http://static.alipayobjects.com/'
-  var ARALE_BASE = 'http://aralejs.org/source/'
-  var PACKAGE = {}
+  var ALIPAY_BASE = 'http://static.alipayobjects.com/';
+  var ARALE_BASE = 'http://aralejs.org/source/';
 
-  var mapRules = []
+  var mapRules = [], coverRule = [];
   mapRules.push(function(url) {
 
     // CDN_MODULES 直接从 alipay 的 cdn 上加载
     for (var i = 0; i < CDN_MODULES.length; i++) {
       if (url.indexOf(CDN_MODULES[i] + '/') > 0) {
-        return url.replace(ARALE_BASE, ALIPAY_BASE)
+        return url.replace(ARALE_BASE, ALIPAY_BASE);
       }
     }
 
@@ -23,27 +22,20 @@
         || (seajs._package && seajs._package.root !== 'arale'))
         && url.indexOf(ARALE_BASE) != -1) {
         // 链接转换成 http://arale.alipay.im/source/overlay/0.9.9/overlay.js
-        url = url.replace(ARALE_BASE, 'http://aralejs.alipay.im/source/')
+        url = url.replace(ARALE_BASE, 'http://aralejs.alipay.im/source/');
         return url;
     } 
-
-    // 如果是线上demo，则走下面的流程
-    
-    /* 原来转换 github 的链接地址的，现在不需要了
-
-    url = url.replace(ARALE_BASE + 'arale/', ARALE_BASE);
-
-    // 将 "/master/xxx.js" 转换成 "/master/dist/xxx.js"
-    url = url.replace(/\/master\/([^\/]+\.js)$/, '/master/dist/$1')
-
-    // 将 "/1.0.2/xxx.js" 转换成 "/1.0.2/dist/xxx.js"
-    url = url.replace(/\/([\d\.]+)\/([^\/]+\.js)$/, '/$1/dist/$2')
-
-    */
-
-    return url
+    return url;
 
   })
+
+  if (location.search === '?coverage=1') {
+    coverRule.push(function(url) {
+      url = url.replace('/src/', '/src-cov/');
+      return url;
+    });
+    mapRules = mapRules.concat(coverRule);
+  }
 
   seajs.config({
     base: ARALE_BASE,
@@ -61,7 +53,8 @@
   })
   if (seajs._nicodebug) {
     seajs.config({
-      base: '/sea-modules'
+      base: '/sea-modules',
+      map: coverRule
     })
   } else {
     seajs.config({
